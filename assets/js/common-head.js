7ad14ad1bd194c836d1cc3331f4a25a31e693f30
wina.finka1 <wina.finka@dkatalis.com>
2023-12-29 00:31:42 +0700

    fix: favicon

--- assets/js/common-head.js
+++ assets/js/common-head.js
@@ -5,4 +5,5 @@ document.write('<title>Wina Finka</title>');
 document.write('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.2/dist/lux/bootstrap.min.css">');
 document.write('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">');
 document.write('<link rel="icon" type="image/x-icon" href="assets/img/favicon.ico">');
+document.write('<link rel="icon" type="image/x-icon" href="../assets/img/favicon.ico">');
 // Add any other common head elements here
