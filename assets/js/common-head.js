caed63cfbb91d591fa5fef5df62128eb2a88c059
wina.finka1 <wina.finka@dkatalis.com>
2023-12-29 00:29:31 +0700

    fix: bootswatch using cdn

--- assets/js/common-head.js
+++ assets/js/common-head.js
@@ -2,8 +2,7 @@
 document.write('<meta charset="UTF-8">');
 document.write('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
 document.write('<title>Wina Finka</title>');
-document.write('<link rel="stylesheet" href="node_modules/bootswatch/dist/lux/bootstrap.min.css">');
-document.write('<link rel="stylesheet" href="../node_modules/bootswatch/dist/lux/bootstrap.min.css">');
+document.write('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.2/dist/lux/bootstrap.min.css">');
 document.write('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">');
 document.write('<link rel="icon" type="image/x-icon" href="assets/img/favicon.ico">');
 // Add any other common head elements here
